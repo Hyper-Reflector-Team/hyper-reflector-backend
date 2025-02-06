0339753cf534c418d96e6fffb5428fe16288d514
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-06 18:01:31 +0900

    fixes test command import

--- src/express-server/express.ts
+++ src/express-server/express.ts
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const fbCommands = require('./firebase')
 
 app.get('/test', (req, res) => {
     res.send('Hello World!')
-    testCommand('weeeeeee')
+    fbCommands.testCommand('weeeeeee')
     console.log('hey we got a reques', JSON.stringify(req))
 })
 
