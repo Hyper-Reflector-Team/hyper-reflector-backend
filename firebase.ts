f039e084a77f40b4b0dd22b49eccfecd27c79bd8
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-06 15:16:26 +0900

    adds ts and other packages, adds node_modules to git ignore

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,30 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const { getFirestore, Timestamp, FieldValue, Filter } = import('firebase-admin/firestore');
 
-const serviceAccount = require('./path/to/serviceAccountKey.json');
+const serviceAccount = import('./keys/service_account_key.json');
 initializeApp({
-  credential: cert(serviceAccount)
+    credential: cert(serviceAccount)
 });
 
 const db = getFirestore();
+
+const docRef = db.collection('users').doc('alovelace');
+
+await docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+});
+
+// export { }
+
+// try {
+//     const docRef = await addDoc(collection(db, "users"), {
+//       first: "Ada",
+//       last: "Lovelace",
+//       born: 1815
+//     });
+//     console.log("Document written with ID: ", docRef.id);
+//   } catch (e) {
+//     console.error("Error adding document: ", e);
+//   }
