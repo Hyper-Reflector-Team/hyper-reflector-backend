58fdaf978cad5a31ad801f54800516b629ad6a89
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-06 19:26:41 +0900

    adds id token check before we make any requests

--- src/express-server/express.ts
+++ src/express-server/express.ts
@@ -3,8 +3,10 @@ const app = express()
 const port = 8080
 // firebase
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 const serviceAccount = require('../../keys/service_account_key.json');
+
 initializeApp({
     credential: cert(serviceAccount)
 });
@@ -15,7 +17,6 @@ const docRef = db.collection('users').doc('alovelace');
 
 // Write new user to db
 async function testCommand(name) {
-    console.log(docRef)
     await docRef.set({
         first: name,
         last: 'Lovelace',
@@ -23,17 +24,26 @@ async function testCommand(name) {
     });
 }
 
-
+app.use(express.json()) // for parsing application/json
 
 
 // SERVER
-app.get('/test', (req, res) => {
+app.post('/test', (req, res) => {
+    console.log(req.body)
+    getAuth().verifyIdToken(req.body.idToken).then(() => {
+        console.log('found user')
+    }).catch(err => {
+        console.log('user touched api without being logged in', err)
+    })
     res.send('Hello World!')
     testCommand('weeeeeee')
     console.log('hey we got a request')
-    console.log(req)
+    // console.log(req)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
