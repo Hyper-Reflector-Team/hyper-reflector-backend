27e11acc7ad4be360780b3545039329a1430fea5
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-06 18:06:13 +0900

    fixes server command file, need to fix ts

--- src/express-server/express.ts
+++ src/express-server/express.ts
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const fbCommands = require('./firebase')
+// firebase
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const serviceAccount = require('../../keys/service_account_key.json');
+initializeApp({
+    credential: cert(serviceAccount)
+});
 
 app.get('/test', (req, res) => {
     res.send('Hello World!')
@@ -12,3 +18,18 @@ app.get('/test', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+// firebase related commands 
+const db = getFirestore();
+const docRef = db.collection('users').doc('alovelace');
+
+// Write new user to db
+async function testCommand(name) {
+    console.log(docRef)
+    await docRef.set({
+        first: name,
+        last: 'Lovelace',
+        born: 1815
+    });
+}
