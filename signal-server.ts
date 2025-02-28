5d5b0f1568802ecd801080223dccb9a53d4a326b
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-28 14:48:35 +0900

    socket server updates

--- signal-server.ts
+++ signal-server.ts
@@ -34,12 +34,14 @@ wss.on('connection', (ws) => {
 
         // handle user making call
         if (data.type === 'callUser') {
-            console.log('socket recieved request to call')
-            const { callerId, calleeId, offer } = data
+            const { callerId, calleeId, localDescription } = data.data
+            console.log(connectedUsers)
+            console.log('data - ', data)
+            console.log('socket recieved request to call from - ', callerId, ' to ', calleeId)
             if (connectedUsers.has(calleeId)) {
                 // get the user we are calling from the user list
                 const callee = connectedUsers.get(calleeId)
-                callee.ws.send(JSON.stringify({ type: 'incomingCall', callerId, offer }))
+                callee.ws.send(JSON.stringify({ type: 'incomingCall', callerId, offer: localDescription })) // local description is the offer
             } else {
                 ws.send(JSON.stringify({ type: 'error', message: 'user not online' }))
             }
@@ -48,7 +50,7 @@ wss.on('connection', (ws) => {
         // handle user answer call
         if (data.type === 'answerCall') {
             console.log('socket call request was answered')
-            const { callerId, answer } = data
+            const { callerId, answer } = data.data
             if (connectedUsers.has(callerId)) {
                 // if caller exists we get them by id from user list
                 const caller = connectedUsers.get(callerId)
@@ -58,7 +60,9 @@ wss.on('connection', (ws) => {
 
         // handle ice candidate exchanging
         if (data.type === 'iceCandidate') {
+            console.log('we got an ice candidate', data)
             const { targetId, candidate } = data
+            console.log('we got an ice candidate', targetId, candidate)
             if (connectedUsers.has(targetId)) {
                 const targetUser = connectedUsers.get(targetId)
                 targetUser.ws.send(JSON.stringify({ type: 'iceCandidate', candidate }))
