867e774a1a7131e5060294abf89c86b04e19af96
Dustin Walker! <dustinwalkerart@gmail.com>
2025-05-02 14:04:33 +0900

    adds further logging

--- src/v2/v3Signal.ts
+++ src/v2/v3Signal.ts
@@ -20,8 +20,7 @@ wss.on('connection', (ws) => {
 
         if (data.type === 'join') {
             user = data.user
-            console.log(user)
-            console.log('User connected -', user.email)
+            // console.log(user)
 
             if (!connectedUsers.has(user.uid)) {
                 connectedUsers.set(user.uid, { ...user, ws })
@@ -126,10 +125,11 @@ wss.on('connection', (ws) => {
 
         if (data.type === 'callUser') {
             const { callerId, calleeId, localDescription } = data.data
-            console.log(connectedUsers)
+            //console.log(connectedUsers)
             console.log('data - ', data)
             console.log('socket recieved request to call from - ', callerId, ' to ', calleeId)
             if (connectedUsers.has(calleeId)) {
+                console.log('sending a call off to user', calleeId)
                 // get the user we are calling from the user list
                 const callee = connectedUsers.get(calleeId)
                 callee.ws.send(
