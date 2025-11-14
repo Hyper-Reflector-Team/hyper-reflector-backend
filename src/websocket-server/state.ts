cc0698cf5806a6830fa08b32e69e24d0cdd708dc
Dustin Walker! <dustinwalkerart@gmail.com>
2025-11-14 17:57:37 +0900

    fixes typo

--- src/websocket-server/state.ts
+++ src/websocket-server/state.ts
@@ -3,7 +3,7 @@ import { ConnectedUser, LobbyMeta } from './types'
 export const connectedUsers = new Map<string, ConnectedUser>()
 export const lobbies = new Map<string, Map<string, ConnectedUser>>()
 export const lobbyMeta = new Map<string, LobbyMeta>()
-export const lobbyTimeouts = new Map<string, NodeJS.Timeout>>()
+export const lobbyTimeouts = new Map<string, NodeJS.Timeout>()
 export const userLobby = new Map<string, string>()
 export const activeMatches = new Map<
     string,
